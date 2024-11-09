271990242fa57c0d78f59401a94ac15efebae212
Elizabeth <lizagotovitryby@gmail.com>
2024-11-09 14:59:01 -0500

    add static files

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const { PORT, BACKEND_URL,CORS_ORIGIN } = process.env;
+
+// cors permission
+app.use(cors({ origin: CORS_ORIGIN }));
+
+// requests parcing
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+   res.status(200).json({message: "Hiiii"})
+  });
+
+// processing unsupported routes
+app.use((req, res ) => {
+    res.status(404).json({
+        message: "Route not found",
+    });
+});
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is listening at ${BACKEND_URL}:${PORT}`);
+  });
